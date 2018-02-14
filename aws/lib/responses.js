845fb2e358538f31afb2b9c903ae856634111566
Linda Nichols <lynnaloo@gmail.com>
2018-02-14 00:44:08 -0500

    [update] add responses

--- aws/lib/responses.js
+++ aws/lib/responses.js
@@ -4,5 +4,6 @@ module.exports = [
   'Happy Valentine\'s Day! You\'ll never be null in my interpreter.',
   'Happy Valentine\'s Day! My response to you is never undefined.',
   'Happy Valentine\'s Day! Our friendship has no bugs!',
-  'Happy Valentine\'s Day! You\'re my favorite human.'
+  'Happy Valentine\'s Day! You\'re my favorite human.',
+  'Happy Valentine\'s Day! We connect like good wifi!'
 ];
